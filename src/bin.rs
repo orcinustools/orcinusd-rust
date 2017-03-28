6cabc8b1bf2da85d5fdea91e6875ef4fda6ceeb7
anak10thn <anak10thn@gmail.com>
2017-03-28 13:00:00 +0700

    ENABLE: web mode

--- src/bin.rs
+++ src/bin.rs
@@ -1,14 +1,13 @@
 extern crate orcinusd;
 
-//use orcinusd::orcinus::Init;
-use orcinusd::utils;
+// use orcinusd::utils;
+// use orcinusd::orcinus::Init;
 use orcinusd::http::server;
-use orcinusd::orcinus::Init;
 
 fn main() {
-    let init = Init::sock("/var/run/docker.sock");
-    let json_swarm = utils::to_json(&init.cluster().info());
-    println!("{}",&json_swarm);
-    //web();
+    // let init = Init::sock("/var/run/docker.sock");
+    // let json_swarm = utils::to_json(&init.cluster().info());
+    // println!("{}",&json_swarm);
+
     server("127.0.0.1:8000");
 }
